695d78a25984304382ca1e01de2a4fa6e5c72058
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-03-18 17:12:23 -0300

    first commit

---
+++ src/main/java/me/dio/api_nuvem/ApiNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNuvemApplication.class, args);
+	}
+
+}
