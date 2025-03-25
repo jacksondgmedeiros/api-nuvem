d24dda6c21efa08fde9d12d71f798308c24cc682
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-03-25 17:51:54 -0300

    adicionando o arquivo procfile e configurando as informações do banco para build no Railway

--- src/main/java/me/dio/api_nuvem/ApiNuvemApplication.java
+++ src/main/java/me/dio/api_nuvem/ApiNuvemApplication.java
@@ -1,8 +1,13 @@
 package me.dio.api_nuvem;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//configuração de swagger para remover o erro de CORS
+//@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ApiNuvemApplication {
 
